6ee8c2c01c528da3b8306098367aaf6c563ee95b
Joshin10 <76895906+Joshin10@users.noreply.github.com>
2021-02-11 16:00:21 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,10 @@
+Class Ground extends BaseClass{
+  constructor(x,y,width,height){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/Ground.png");
+  
+  }
+  /*display(){
+    super.display();
+  }*/
+}
